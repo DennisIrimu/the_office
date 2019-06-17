08436c7657d28abce918c771db27d480ca3bb2fd
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-17 22:51:42 +0300

    Created class User methods

--- accounts/models.py
+++ accounts/models.py
@@ -6,11 +6,12 @@ from django.contrib.auth.models import (
 
 
 class User(AbstractBaseUser):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
+    full_name = models.CharField(max_length=150,unique=True)
+    email = models.EmailField(verbose_name='email address',max_length=255,unique=True)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False) # a admin user; non super-user
     admin = models.BooleanField(default=False) # a superuser
+
+    USERNAME_FIELD = 'full_name'
+
+    REQUIRED_FIELDS = ['fullname']
