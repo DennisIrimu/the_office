134d1ad2367bc38f13ae762e384b57a33f10fffe
DennisIrimu <lyricaldnyt@gmail.com>
2019-06-17 23:08:31 +0300

    Created UserManager class to instantiate user objects

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,33 @@ from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError('You need a valid email address')
+        if not password:
+            raise ValueError('Incorrect Password')
+
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+    
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,password=password,is_staff=True
+        )
+        return user
+    
+    def create_superuser(self, email, password=None):
+         user = self.create_user(
+            email,password=password,is_staff=True,is_admin=True
+        )
+
 
 
 class User(AbstractBaseUser):
@@ -11,7 +38,14 @@ class User(AbstractBaseUser):
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False) # a admin user; non super-user
     admin = models.BooleanField(default=False) # a superuser
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'full_name'
 
     REQUIRED_FIELDS = ['fullname']
+
+    def get_username(self):
+        return self.full_name
+
+    def __str__(self):
+        return self.full_name
